82836b75fb71665b7058ea2867fd4e9e4d979ee7
Ubuntu <ubuntu@ip-172-31-27-203.us-east-2.compute.internal>
2021-07-06 01:49:40 +0000

    refactored processed_content

--- lib/codyjamesbrooks_palindrome.rb
+++ lib/codyjamesbrooks_palindrome.rb
@@ -5,17 +5,10 @@ class String
   def palindrome?
     processed_content == processed_content.reverse
   end
-  
-  # Returns the letters in the string.
-  def letters
-    the_letters = ""
-    the_letter = self.split(//).select { |letter| letter.match?(/[a-zA-Z]/) }
-    the_letter.join
-  end 
-  
+
   private
     # Retturns content for palindrome testing. 
     def processed_content
-      self.letters.downcase
+      scan(/[a-z]/i).join.downcase
     end
 end
